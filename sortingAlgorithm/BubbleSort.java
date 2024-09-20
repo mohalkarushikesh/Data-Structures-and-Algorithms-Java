12a7f5d6ebb99f4668712b48b8a08e40fd7ed3b0
Rushikesh Mohalkar <82998291+mohalkarushikesh@users.noreply.github.com>
2024-09-21 01:26:06 +0530

    Update BubbleSort.java

--- sortingAlgorithm/BubbleSort.java
+++ sortingAlgorithm/BubbleSort.java
@@ -12,25 +12,35 @@ Visualization: Imagine bubbles rising to the surface, with the largest bubble (e
 // 1. check arr[j] > arr[j+1]
 // yes swap elements
 
-class BubbleSort{
-	public static void main(String[] args){
-		int arr[] = {4, 1, 55, 32, 44 ,776, 64, 77, 99};
-		Bubblesort(arr);		
-		for(int i: arr){
-			System.out.print(i+" ");
+cpackage sortingAlgorithms;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int arr[] = {3, 2, 5, 1, 6, 0, 4, 5, 9};
+        // Call the Bubblesort function to sort the array
+        Bubblesort(arr);
+        
+        // Print the sorted array
+        for (int i : arr) {
+            System.out.print(i + " ");
         }
     }
-    public static void Bubblesort(int arr[]){
-		int n = arr.length;
-        for(int i=0;i<n-1;i++){
-		    for(int j=0; j<n-i-1;j++){
-			    if(arr[j] > arr[j+1]){
-					int temp=arr[j];
-					arr[j]=arr[j+1];
-					arr[j+1]=temp;
+
+    public static void Bubblesort(int arr[]) {
+        int n = arr.length;
+        // Outer loop to traverse through all elements
+        for (int i = 0; i < n - 1; i++) { 
+            // Inner loop for comparing adjacent elements
+            for (int j = 0; j < n - i - 1; j++) { 
+                // Swap if the element found is greater than the next element
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
     }
 }
+
 
