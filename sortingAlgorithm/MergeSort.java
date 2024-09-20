3b489eb4cbc7fd4ec18555e9064528275d08cfeb
Rushikesh Mohalkar <82998291+mohalkarushikesh@users.noreply.github.com>
2024-09-21 01:04:28 +0530

    Update MergeSort.java

--- sortingAlgorithm/MergeSort.java
+++ sortingAlgorithm/MergeSort.java
@@ -1,3 +1,13 @@
+/*
+Merge Sort
+Concept: Divide and conquer by splitting the list into halves.
+Steps:
+Divide the list into two halves.
+Recursively sort each half.
+Merge the two sorted halves back together.
+Visualization: Think of splitting a deck of cards into smaller piles, sorting each pile, and then merging them back together.
+*/
+
 class MergeSort{
     public static void main(String[] args){
         int arr[] = {4, 22, 33, 75, 3, 0, 3, 9, 32};
