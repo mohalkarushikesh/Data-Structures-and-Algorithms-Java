4934c2421d619f852261a24236cee16baddff1f7
Rushikesh Mohalkar <82998291+mohalkarushikesh@users.noreply.github.com>
2024-09-21 01:29:48 +0530

    Update SelectionSort.java

--- sortingAlgorithm/SelectionSort.java
+++ sortingAlgorithm/SelectionSort.java
@@ -15,30 +15,36 @@ Visualization: Imagine selecting the smallest item from a list and moving it to 
 //          fin min
 //          update min
 //      swap arr[i],arr[min]
- 
-public class SelectionSort {
+ public class SelectionSort {
     public static void main(String[] args) {
-      int arr[] = {9, 1, 8, 2, 7, 3, 6, 4, 5};
-      
-      selectionsort(arr);
-      
-      for(int i: arr){
-        System.out.print(i+ " ");
-      }
-  }
-   public static void selectionsort(int arr[]){
-     for(int i=0; i<arr.length-1;i++){
-       int min = i; // current min 
-       for(int j=i+1; j<arr.length;j++){ //inner loop
-         if(arr[j]<arr[min]){ // find min 
-           min = j; // update min 
-         }
-       }
-        // swap i with min
-        int temp=arr[i];
-        arr[i]=arr[min];
-        arr[min]=temp;
-     }
-     
-   }
+        int arr[] = {9, 1, 8, 2, 7, 3, 6, 4, 5};
+        
+        // Call the selectionsort function to sort the array
+        selectionsort(arr);
+        
+        // Print the sorted array
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void selectionsort(int arr[]) {
+        // Outer loop to traverse through all elements
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i; // Assume the current element is the minimum
+            
+            // Inner loop to find the minimum element in the unsorted part
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) { // If a smaller element is found
+                    min = j; // Update the index of the minimum element
+                }
+            }
+            
+            // Swap the found minimum element with the first element of the unsorted part
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
 }
+
