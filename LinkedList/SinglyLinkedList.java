bb437db7adb7007db2fa3da66794be00d8a7a9d1
Rushikesh Mohalkar <82998291+mohalkarushikesh@users.noreply.github.com>
2025-01-10 15:22:39 +0530

    Update SinglyLinkedList.java

--- LinkedList/SinglyLinkedList.java
+++ LinkedList/SinglyLinkedList.java
@@ -1,132 +1,235 @@
 package LinkedList;
 
+import org.jcp.xml.dsig.internal.SignerOutputStream;
+
 public class SinglyLinkedList {
-	Node head; // head of list
+	Node head;
 
-	static class Node { // Linked list node
+	static class Node {
 		int data;
-		Node next;
+		Node nxt;
 
-		Node(int data) { // constructor
+		Node(int data) {
 			this.data = data;
-			this.next = null;
+			this.nxt = null;
 		}
 	}
 
-	// method to insert new node based on index 
-	public static SinglyLinkedList insertAtPosition(SinglyLinkedList list, int index, int data) {
-		// create new node
-		Node new_node = new Node(data);
-
-		if (list.head == null && index == 0) { 
-			new_node.next = list.head; //  sets the next pointer of the new node to the current head of the list. Essentially, it links the new node to the existing list.
-			list.head = new_node;	// set newNode to head 
-			System.out.println("At index " + index + " inserted " + data);
+	// index based insertion
+	public static SinglyLinkedList insertAtAnyPosition(SinglyLinkedList list, int index, int data) {
+		Node newNode = new Node(data);
+		if (list.head == null && index == 0) {
+			newNode.nxt = list.head;
+			list.head = newNode;
+			System.out.println("at index " + index + " inserted " + data + " Basically inserted at the beginning");
 			return list;
 		}
-
+		// other index
 		Node currNode = list.head;
 		Node prev = null;
 		int counter = 0;
 		while (currNode != null && counter < index) {
-			prev = currNode;	// prev node updated to currNode 
-			currNode = currNode.next;	// currNode updated to next node 
+			prev = currNode;
+			currNode = currNode.nxt;
 			counter++;
 		}
-
-		new_node.next = currNode;
-		if (prev != null) { // it means the new node is not being inserted at the head, so prev.next is set to the new node.
-			prev.next = new_node;	// here insert operation happens
+		newNode.nxt = currNode;
+		if (prev != null) {
+			prev.nxt = newNode;
 		}
-
-		System.out.println("At index " + index + " inserted " + data);
+		System.out.println("at index " + index + " inserted " + data);
 		return list;
 	}
 
-	// delete node based on index
+	// index based deletion
 	public static SinglyLinkedList deleteAtPosition(SinglyLinkedList list, int index) {
 		Node currNode = list.head, prev = null;
 
-		if (index == 0 && currNode != null) {
-			list.head = currNode.next;
-			System.out.println("element at head is deleted!");
-			return list;
+		if (currNode != null && index == 0) {
+			list.head = currNode.nxt;
+			System.out.println("at index " + index + " deleted, i.e " + " beginning of the list " + currNode.data
+					+ " is deleted ");
 		}
 
-		// delete in between
-
 		int counter = 0;
-
 		while (currNode != null) {
 			if (index == counter) {
-				prev.next = currNode.next;
-				System.out.println("Index " + index + " is deleted!");
+				if (prev != null) {
+					prev.nxt = currNode.nxt;
+					System.out.println("at index " + index + " element " + currNode.data + " is deleted");
+				}
 				break;
 			} else {
-				prev = currNode; // prev node updated to currNode
-				currNode = currNode.next; // currNode updated to next node
+				prev = currNode;
+				currNode = currNode.nxt;
 				counter++;
 			}
 		}
 
-		// index not found
 		if (currNode == null) {
-			System.out.println("Index " + index + " not found!");
+			System.out.println(index + " not found");
 		}
 
-		return list; // return the updates list
+		return list;
 	}
 
-	// search based on index 
-	public static int search(SinglyLinkedList list, int element) {
+	// insert at the beginning
+	public void insertAtBeginning(int data) {
+		Node newNode = new Node(data);
+		newNode.nxt = head;
+		head = newNode;
+		System.out.println(data + " inserted at the beginning");
+	}
+
+	public void insertAfter(Node prevNode, int data) {
+		// check if prev Node is null or not
+		if (prevNode == null) {
+			System.out.println("prevNode should be not be NULL");
+			return;
+		}
+		Node newNode = new Node(data);
+		newNode.nxt = prevNode.nxt;
+		prevNode.nxt = newNode;
+		System.out.println(newNode.data + " inserted after " + prevNode.data);
+	}
+
+	public void insertatend(int data) { // or else renamed as append method
+		Node newNode = new Node(data);
+		if (head == null) { // if the list is empty
+			newNode.nxt = head;
+			head = newNode;
+		}
+		Node last = head;
+		while (last.nxt != null) {
+			last = last.nxt;
+		}
+		last.nxt = newNode;
+		System.out.println(newNode.data + " inserted at the end ");
+	}
+
+	public void deleteFirstNode() {
+		if (head == null) {
+			System.out.println("list is empty");
+			return;
+		}
+		System.out.println(head.data + " The element at the beginning is deleted ");
+		head = head.nxt;
+
+	}
+
+	public void deleteMidddleNode(Node prevNode) {
+		if (prevNode == null || prevNode.nxt == null) {
+			System.out.println("No need to delete after the given node");
+			return;
+		}
+		Node nodeToDelete = prevNode.nxt;
+		prevNode.nxt = nodeToDelete.nxt;
+		System.out.println("deleted node " + nodeToDelete.data + " after the given node " + prevNode.data);
+	}
+
+	public void deleteEndNode() {
+		if (head == null) {
+			System.out.println("list is empty");
+			return;
+		}
+		if (head.nxt == null) { // if only one element
+			head = null;
+			return;
+		}
+		Node secLast = head;
+		while (secLast.nxt.nxt != null) {
+			secLast = secLast.nxt;
+		}
+		System.out.println(secLast.nxt.data + " is deleted i.e is  last element is deleted");
+		secLast.nxt = null;
+
+	}
+
+	public int searchByValue(SinglyLinkedList list, int element) {
 		if (list.head == null) {
 			return -1;
 		}
 		Node temp = list.head;
 		int index = 0;
 		while (temp != null) {
-			if (element == temp.data) {
+			if (temp.data == element) {
 				return index;
 			}
+			temp = temp.nxt;
 			index++;
-			temp = temp.next;
 		}
 		return -1;
 	}
 
-	// printing linked list
-	public static void printList(SinglyLinkedList list) {
-		Node currNode = list.head; // currently head is current node
-		System.out.print("Linked list: ");
-		while (currNode != null) {
-			System.out.print(currNode.data + " ");
-			currNode = currNode.next; // move to next node
+	public static void searchByValue() {
+
+	}
+
+	public void printlist() {
+		Node temp = head;
+		System.out.print("Linked List: ");
+		while (temp != null) {
+			System.out.print(temp.data + " ");
+			temp = temp.nxt;
 		}
 		System.out.println();
 	}
 
 	public static void main(String[] args) {
+
+		System.out.println("****************Singly Linked List operations*************");
+		System.out.println();
+
 		SinglyLinkedList list = new SinglyLinkedList();
 
-		list = insertAtPosition(list, 0, 10);
-		list = insertAtPosition(list, 1, 20);
-		list = insertAtPosition(list, 2, 30);
-		list = insertAtPosition(list, 4, 50);
+		list = insertAtAnyPosition(list, 0, 10);
+		list = insertAtAnyPosition(list, 1, 20);
+		list = insertAtAnyPosition(list, 2, 30);
+		list = insertAtAnyPosition(list, 4, 50);
+		list = insertAtAnyPosition(list, 5, 60);
+		list = insertAtAnyPosition(list, 6, 70);
+		list.printlist();
 
-		printList(list);
+		System.out.println();
 
-		list = insertAtPosition(list, 3, 40);
+		list = insertAtAnyPosition(list, 3, 40);
+		list.printlist();
+		System.out.println();
+		list = deleteAtPosition(list, 0);
+		list.printlist();
+		System.out.println();
+		list = deleteAtPosition(list, 5);
+		list.printlist();
+		System.out.println();
+		list.insertAtBeginning(9);
+		list.printlist();
+		System.out.println();
+		Node fourth = list.head.nxt.nxt.nxt;
 
-		// printList(list);
+		list.insertAfter(fourth, 45);
+		list.printlist();
+		System.out.println();
+		list.insertatend(70);
+		list.printlist();
+		System.out.println();
+		list.deleteFirstNode();
+		list.printlist();
+		System.out.println();
+		Node fortyFive = list.head.nxt.nxt;
 
-		int element = 50;
-
-		int ans = search(list, element);
-
-		if (ans == -1) {
-			System.out.println("element not found in liked list");
+		list.deleteMidddleNode(fortyFive);
+		list.printlist();
+		System.out.println();
+		list.deleteEndNode();
+		list.printlist();
+		System.out.println();
+		int index = list.searchByValue(list, 60);
+		if (index == -1) {
+			System.out.println("Element not found in list");
 		} else {
-			System.out.println("element found at the index " + ans);
+			System.out.println("Element found at index " + index);
 		}
+		list.printlist();
+		System.out.println();
 	}
 }
