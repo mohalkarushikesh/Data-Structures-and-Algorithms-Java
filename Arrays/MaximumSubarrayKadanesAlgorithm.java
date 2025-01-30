942fd40b86ea0f5f8f69aef83a7d4ca27750924b
Rushikesh Mohalkar <82998291+mohalkarushikesh@users.noreply.github.com>
2025-01-30 20:20:49 +0530

    Create MaximumSubarrayKadanesAlgorithm.java

---
+++ Arrays/MaximumSubarrayKadanesAlgorithm.java
@@ -0,0 +1,35 @@
+// bruteforce
+int maxi = Integer.MIN_VALUE;
+for(0 to n){
+	for(j=i to n){
+		int sum = 0
+		for(k=i to k<=j){
+			sum = arr[k];
+		}
+		maxi = Math.max(maxi, sum);
+	}
+	return maxi;
+}
+
+// better
+int maxi = Integer.MIN_VALUE;
+for( 0 to n){
+	for(j=i to n){
+		int sum = 0;
+		sum = sum + arr[j];
+		maxi = Math.max(maxi, sum);
+	}
+	return maxi;
+}
+
+// optimal sol
+for(0 to n){
+	int sum = 0;
+	int maxi = Integer.MAX_VALUE;
+	sum = sum + arr[i];
+	if(sum>maxi)
+		maxi = sum;
+	if(sum<0) // empty subarr
+		sum=0;
+	return maxi;
+}
